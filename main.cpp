5cca04b51b95cbd86ed9ca0e501faba483316f0f
Du Park <dupark3@gmail.com>
2018-03-22 16:55:28 -0400

    Changed Student_info to Core. Still type-dependent

--- main.cpp
+++ main.cpp
@@ -7,18 +7,23 @@ Compile, execute, and test the programs in this chapter.
 #include <algorithm> // sort, max
 #include <cstddef> // size_t
 #include <iostream>
+#include <stdexcept> // domain_error
 #include <string>
 #include <vector>
 
 #include "Student_info.h"
 
+typedef std::vector<Core>::size_type vec_size;
+typedef std::string::size_type string_size;
+
 int main() {
     std::cout << "Enter each student's name, midterm grade, final grade, and homework grades: " << std::endl;
-    Student_info record;
-    std::vector<Student_info> students;
-    size_t maxLen = 0;
 
-    // read and store
+    std::vector<Core> students;
+    Core record;
+    string_size maxLen = 0;
+
+    // read and store the data
     while(record.read(std::cin)){
         maxLen = std::max(maxLen, record.name().size());
         students.push_back(record);
@@ -28,16 +33,17 @@ int main() {
     std::sort(students.begin(), students.end(), compare);
 
     // output
-    for (size_t i = 0; i != students.size(); ++i){
-        std::cout << students[i].name() << std::string(maxLen + 1 - students[i].name().size(), ' ')
-                  << students[i].letterGrade() << std::endl;
+    for (vec_size i = 0; i != students.size(); ++i){
+        std::cout << students[i].name() 
+                  << std::string(maxLen + 1 - students[i].name().size(), ' ');
+        try {
+            double final_grade = students[i].grade(); // Core::grade
+            streamsize prec = std::cout.precision();
+            cout << setprecision(3) << final_grade 
+                 << setprecision(prec) << std::endl;
+        } catch (std::domain_error e) {
+            std::cout << e.what() << std::endl;
+        }
     }
-
-    // testing if the median function can take an array instead of a vector
-    int integers[] = {12, 5, 6, 10, 10, 3}; // median = 8
-    int middle = 0;
-    median(integers, integers + sizeof(integers) / sizeof(*integers), middle); // second iterator is the first iterator + size of array
-    std::cout << "Array's median: " << middle << std::endl;
-
     return 0;
 }
